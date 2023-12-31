6f729e434986b0e615faf465ab60972705642c93
Arina <arinapostnikova123@gmail.com>
2023-12-31 18:59:54 +0300

    navbar fix

--- GitHubFollowers/Screens/SearchVC.swift
+++ GitHubFollowers/Screens/SearchVC.swift
@@ -28,7 +28,7 @@ class SearchVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        navigationController?.isNavigationBarHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
     }
     
     func createDismissKeyboardTapGesture() {
@@ -39,7 +39,7 @@ class SearchVC: UIViewController {
     @objc
     func pushFollowerListVC() {
         guard isUsernameEntered else {
-            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to looking for 🙂", buttonTitle: "Ok")
+            presentGFAlertOnMainThread(title: "Empty Username", message: "Please enter a username. We need to know who to looking for 🙂.", buttonTitle: "Ok")
             return
         }
         
