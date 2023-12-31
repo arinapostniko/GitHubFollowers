45b98be4d6f90e10957589db1ff621ab68963887
Arina <arinapostnikova123@gmail.com>
2023-12-31 13:17:49 +0300

    create custom Button and TextField

---
+++ GitHubFollowers/MainButton.swift
@@ -0,0 +1,39 @@
+//
+//  MainButton.swift
+//  GitHubFollowers
+//
+//  Created by Arina Postnikova on 31/12/23.
+//
+
+import UIKit
+
+class MainButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
