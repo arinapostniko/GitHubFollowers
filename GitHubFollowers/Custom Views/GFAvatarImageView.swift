2a8b152160a358d5ff5fb65d4b14d43ec6ba0da7
Arina <arinapostnikova123@gmail.com>
2023-12-31 21:52:07 +0300

    create FollowerCell

---
+++ GitHubFollowers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,28 @@
+//
+//  GFAvatarImageView.swift
+//  GitHubFollowers
+//
+//  Created by Arina Postnikova on 31/12/23.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
