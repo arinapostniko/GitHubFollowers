c53b78308ce7bc17796b042fc388b685fb8901f3
Arina <arinapostnikova123@gmail.com>
2023-12-31 18:54:31 +0300

    create Follower and User models

---
+++ GitHubFollowers/Model/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Follower.swift
+//  GitHubFollowers
+//
+//  Created by Arina Postnikova on 31/12/23.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
