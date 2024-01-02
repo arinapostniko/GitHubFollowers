4fd779cfc180f63edfefa32c2aa0fab756175f3c
Arina <arinapostnikova123@gmail.com>
2024-01-02 14:58:38 +0300

    configure UserInfo header view

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Arina Postnikova on 2/1/24.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
