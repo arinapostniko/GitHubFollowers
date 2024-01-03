87a1270b2480eb11875142a32be5cda46dde1d69
Arina <arinapostnikova123@gmail.com>
2024-01-03 12:52:42 +0300

    configure open GitHub profile button

--- GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
+++ GitHubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFItemInfoVC.swift
@@ -15,12 +15,14 @@ class GFItemInfoVC: UIViewController {
     let actionButton = GFButton()
     
     var user: User!
+    weak var delegate: UserInfoVCDelegate!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
         layoutUI()
         configureStackView()
+        configureActionButton()
     }
     
     init(user: User) {
@@ -44,6 +46,13 @@ class GFItemInfoVC: UIViewController {
         stackView.addArrangedSubview(itemInfoViewOne)
         stackView.addArrangedSubview(itemInfoViewTwo)
     }
+    
+    private func configureActionButton() {
+        actionButton.addTarget(self, action: #selector(actionButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc
+    func actionButtonTapped() {}
     
     private func layoutUI() {
         view.addSubview(stackView)
